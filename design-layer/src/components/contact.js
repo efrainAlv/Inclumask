8dd624992638d0a6d31b990a85aa384ff2964744
efrainAlv <58543968+efrainAlv@users.noreply.github.com>
2021-10-14 00:15:48 -0600

    Update contact.js

--- design-layer/src/components/contact.js
+++ design-layer/src/components/contact.js
@@ -67,22 +67,15 @@ class Contact extends Component {
                                         </i>
                                     </div>
 
-
-                                    <div style={{ margin: "4px" }}>
-                                        <h1>
-                                            Direccion
-                                        </h1>
-                                        <i style={{ fontSize: "20px" }}>
-                                            12 calle 1-25, zona 10. Geminis 10. Torre Norte. Nivel 6. OΟcina 800. Guatemala.
-                                        </i>
-                                    </div>
-
                                     <div style={{ margin: "4px" }}>
                                         <h1>
                                             Facebook
                                         </h1>
                                         <i style={{ fontSize: "20px" }}>
-                                            <img src="images/facebook.svg" alt="alt" style={{ width: "10%" }} />
+                                            <a href="https://www.facebook.com/Inclumask-GT-103542238782668">
+                                                <img src="images/facebook.svg" alt="alt" style={{ width: "10%" }} />
+                                            </a>
+                                            
                                         </i>
                                     </div>
 
@@ -91,7 +84,9 @@ class Contact extends Component {
                                             Instagram
                                         </h1>
                                         <i style={{ fontSize: "20px" }}>
-                                            <img src="images/instagram.png" alt="alt" style={{ width: "10%" }} />
+                                            <a href="https://www.instagram.com/inclumask/">
+                                                <img src="images/instagram.png" alt="alt" style={{ width: "10%" }} />   
+                                            </a>
                                         </i>
                                     </div>
 
